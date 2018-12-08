a429657ba03d1df9091f3ba5f5a285a96bfa698d
stelmakhivan <stelmahivan1994@gmail.com>
2018-12-08 19:15:15 +0200

    server changed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
