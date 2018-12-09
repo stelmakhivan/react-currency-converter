a1bd0a78d6afb1630099a51a8cf232990a522618
stelmakhivan <stelmahivan1994@gmail.com>
2018-12-09 12:21:36 +0200

    fixed engine version

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -4,7 +4,6 @@ module.exports = {
     'build/*.html',
     'build/manifest.json',
     'build/favicon.ico',
-    'favicon.ico',
     'build/static/**/!(*map*)'
   ],
   dontCacheBustUrlsMatching: /\.\w{8}\./,
